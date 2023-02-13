b1605480a52bd1bdfea08da7b5f2d43f84ae7210
paolavasileva22 <p.vasileva2018@pmg-pernik.com>
2023-02-13 13:12:36 +0200

    DOG

---
+++ DogApp/DogApp/Abstractions/IDogService.cs
@@ -0,0 +1,78 @@
+﻿using DogApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogApp.Abstractions
+{
+    public interface IDogService
+    {
+        public bool Create(string name, int age, string breed, string picture)
+        {
+        Dog item = new Dog
+        {
+            Name = name,
+            Age = age,
+            Breed = breed,
+            Picture = picture,
+        };
+        _context.Dogs.Add(item);
+        return _context.SaveChanges() != 0;
+            }
+        public bool UpdateDog(int dogId, string name, int age, string breed, string picture)
+        {
+            var dog = GetDogById(dogId);
+            if (dog == default(Dog))
+            {
+                return false;
+            }
+            dog.Name = name;
+            dog.Age = age;
+            dog.Breed = breed;
+            dog.Picture = picture;
+            _context.Update(dog);
+            return _context.SaveChanges() != 0;
+        }
+
+        public List<Dog> GetDogs()
+        {
+            List<Dog> dogs = _context.Dogs.ToList();
+            return dogs;
+        }
+
+        public Dog GetDogById(int dogId)
+        {
+            return _context.Dogs.Find(dogId);
+        }
+
+        public bool RemoveById(int dogId)
+        {
+            var dog = GetDogById(dogId);
+            if (dog == default(Dog))
+            {
+                return false;
+            }
+            _context.Remove(dog);
+            return _context.SaveChanges() != 0;
+        }
+
+        public List<Dog> GetDogs(string searchStringBreed, string searchStringName)
+        {
+            List<Dog> dogs = _context.Dogs.ToList();
+            if (!string.IsNullOrEmpty(searchStringBreed) && !String.IsNullOrEmpty(searchStringName))
+            {
+                dogs = dogs.Where(d => d.Breed.Contains(searchStringBreed) && d.Name.Contains(searchStringName)).ToList();
+            }
+            else if (!string.IsNullOrEmpty(searchStringBreed))
+            {
+                dogs = dogs.Where(d => d.Breed.Contains(searchStringBreed)).ToList();
+            }
+            else if (!string.IsNullOrEmpty(searchStringName))
+            {
+                dogs = dogs.Where(d => d.Name.Contains(searchStringName)).ToList();
+            }
+            return dogs;
+        }
+    }
+}
---
+++ DogApp/DogApp/Services/DogService.cs
@@ -0,0 +1,49 @@
+﻿using DogApp.Abstractions;
+using DogApp.Data;
+using DogApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogApp.Services
+{
+    public class DogService : IDogService
+    {
+        public readonly ApplicationDbContext _context;
+
+        public DogService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public bool Create(string name, int age, string breed, string picture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dog GetDogById(int dogId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Dog> GetDogs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Dog> GetDogs(string searchStringBreed, string searchStringName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveById(int dogId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateDog(int dogId, string name, int age, string breed, string picture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
